5c98b46c0bc189b327d23706b49a1efc80125a62
madhutejaswara reddy <madhutejaswara reddy@madhu>
2023-10-12 14:11:18 +0530

    first commit

---
+++ src/test/java/com/app/TTD/demo/UsernameCode.java
@@ -0,0 +1,18 @@
+package com.app.TTD.demo;
+
+public class UsernameCode {
+public boolean isvalidUsername(String username) {
+		
+		if(username.contains("/") || (username.contains("#")))
+		{
+			return false;
+		}
+		else if(username.contains("123")){
+			return true;
+		}
+		else
+		return true;
+	}
+
+
+}
